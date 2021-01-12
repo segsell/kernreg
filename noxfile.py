f34b002e22c1553ee61b0e5bcb95201985d568c5
Sebastian Gsell <sebastian.gsell93@gmail.com>
2021-01-12 13:05:43 +0100

    Add flake8-annotations to nox session.

--- noxfile.py
+++ noxfile.py
@@ -42,6 +42,7 @@ def lint(session):
     install_with_constraints(
         session,
         "flake8",
+        "flake8-annotations",
         "flake8-bandit",
         "flake8-black",
         "flake8-bugbear",
