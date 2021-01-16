b0838162dfb3800eb2e245816348e2e44afaf991
Sebastian Gsell <sebastian.gsell93@gmail.com>
2021-01-16 20:36:51 +0100

    Rewrite binning test cases. Turn expected outputs into pytest fixtures.

--- tests/test_binning.py
+++ tests/test_binning.py
@@ -1,149 +1,108 @@
 """Test cases for the linear_binning module."""
-from typing import Tuple
+from typing import Any, Callable, Tuple
 
 import numpy as np
 import pytest
 
-
 from kernreg.linear_binning import include_weights_from_endpoints, linear_binning
-
-xcounts_trunc_true = np.array(
-    [
-        38.06786787,
-        75.68048048,
-        75.68468468,
-        75.68048048,
-        75.68048048,
-        75.68408408,
-        75.68108108,
-        75.68048048,
-        75.68348348,
-        188.47687688,
-    ]
-)
-ycounts_trunc_true = np.array(
-    [
-        613.97600002,
-        1146.67162057,
-        1261.41141141,
-        1376.01106211,
-        1490.6743861,
-        1605.41962984,
-        1720.02656711,
-        1834.67715163,
-        1949.42238936,
-        2758.90697905,
-    ]
-)
-xcounts_trunc_false = np.array(
-    [
-        250.21711712,
-        100.90900901,
-        100.90900901,
-        100.90900901,
-        100.90900901,
-        100.90900901,
-        100.90900901,
-        100.90990991,
-        60.49009009,
-        164.92882883,
-    ]
-)
-ycounts_trunc_false = np.array(
-    [
-        3181.46857668,
-        1579.88869049,
-        1783.74494314,
-        1987.60119579,
-        2191.45744844,
-        2395.31370109,
-        2599.16995374,
-        2803.05305305,
-        1769.55160566,
-        1858.50058166,
-    ]
-)
 
 
 @pytest.fixture
-def generate_test_input() -> Tuple[np.ndarray, np.ndarray]:
-    """Reusable parameterization for x and y inputs."""
-    x = np.linspace(2.2, 6.6, 1000)
-    y = np.linspace(10, 30, 1000)
+def output_trunc_true() -> Tuple[np.ndarray, np.ndarray]:
+    """Expected toy output for linear_binning with default parameter truncate=True."""
+    xcounts = np.array(
+        [
+            38.06786787,
+            75.68048048,
+            75.68468468,
+            75.68048048,
+            75.68048048,
+            75.68408408,
+            75.68108108,
+            75.68048048,
+            75.68348348,
+            188.47687688,
+        ]
+    )
 
-    return x, y
+    ycounts = np.array(
+        [
+            613.97600002,
+            1146.67162057,
+            1261.41141141,
+            1376.01106211,
+            1490.6743861,
+            1605.41962984,
+            1720.02656711,
+            1834.67715163,
+            1949.42238936,
+            2758.90697905,
+        ]
+    )
+
+    return xcounts, ycounts
+
+
+@pytest.fixture
+def output_trunc_false() -> Tuple[np.ndarray, np.ndarray]:
+    """Expected toy output for linear_binning with truncate=False."""
+    xcounts = np.array(
+        [
+            250.21711712,
+            100.90900901,
+            100.90900901,
+            100.90900901,
+            100.90900901,
+            100.90900901,
+            100.90900901,
+            100.90990991,
+            60.49009009,
+            164.92882883,
+        ]
+    )
+    ycounts = np.array(
+        [
+            3181.46857668,
+            1579.88869049,
+            1783.74494314,
+            1987.60119579,
+            2191.45744844,
+            2395.31370109,
+            2599.16995374,
+            2803.05305305,
+            1769.55160566,
+            1858.50058166,
+        ]
+    )
+    return xcounts, ycounts
 
 
 @pytest.mark.parametrize(
-    "b, trunc, expected_xcounts, expected_ycounts",
-    [
-        (6, True, xcounts_trunc_true, ycounts_trunc_true),
-        (7, False, xcounts_trunc_false, ycounts_trunc_false),
-    ],
+    "b, truncate, expected",
+    [(6, True, "output_trunc_true"), (7, False, "output_trunc_false")],
 )
 def test_binning_truncate_True_False(
     b: float,
-    trunc: bool,
-    expected_xcounts: np.ndarray,
-    expected_ycounts: np.ndarray,
-    generate_test_input: Tuple[np.ndarray, np.ndarray],
+    truncate: bool,
+    expected: Callable,
+    request: Any,
 ) -> None:
-    """It bins inputs (x and y) with and without truncation of end points."""
+    """It bins x and y. With and without truncation of end points."""
     a, grid = 3, 10
     binwidth = (b - a) / (grid - 1)
-    x, y = generate_test_input
+    x = np.linspace(2.2, 6.6, 1000)
+    y = np.linspace(10, 30, 1000)
 
-    xcounts, ycounts = linear_binning(x, y, grid, a, binwidth, truncate=trunc)
+    xcounts, ycounts = linear_binning(x, y, grid, a, binwidth, truncate=truncate)
+
+    expected_xcounts, expected_ycounts = request.getfixturevalue(expected)
 
     np.testing.assert_almost_equal(xcounts, expected_xcounts, decimal=8)
     np.testing.assert_almost_equal(ycounts, expected_ycounts, decimal=8)
 
 
-# def test_binning_truncate_False(
-#     generate_test_input: Tuple[np.ndarray, np.ndarray]
-# ) -> None:
-#     """Do not truncate endpoints."""
-#     a, b, grid = 3, 7, 10
-#     binwidth = (b - a) / (grid - 1)
-#     x, y = generate_test_input
-
-#     xcounts, ycounts = linear_binning(x, y, grid, a, binwidth, truncate=False)
-
-#     expected_xcounts = np.array(
-#         [
-#             250.21711712,
-#             100.90900901,
-#             100.90900901,
-#             100.90900901,
-#             100.90900901,
-#             100.90900901,
-#             100.90900901,
-#             100.90990991,
-#             60.49009009,
-#             164.92882883,
-#         ]
-#     )
-#     expected_ycounts = np.array(
-#         [
-#             3181.46857668,
-#             1579.88869049,
-#             1783.74494314,
-#             1987.60119579,
-#             2191.45744844,
-#             2395.31370109,
-#             2599.16995374,
-#             2803.05305305,
-#             1769.55160566,
-#             1858.50058166,
-#         ]
-#     )
-
-#     np.testing.assert_almost_equal(xcounts, expected_xcounts, decimal=8)
-#     np.testing.assert_almost_equal(ycounts, expected_ycounts, decimal=8)
-
-
 def test_include_endpoints() -> None:
-    """Calls inner function directly."""
+    """Calls inner function directly, which is otherwise reached if truncate=False."""
     grid = 10
     y = np.linspace(10, 30, 100)
 
